c1710409cfd84f9ca5020bbc4375e9775854fd52
shappy-66 <shappy8866@gmail.com>
2026-07-06 23:32:29 +0800

    add some functions such as 'create file'

---
+++ xv6-riscv/user/filetest.c
@@ -0,0 +1,16 @@
+#include "user/user.h"
+#include "kernel/types.h"
+#include "kernel/fcntl.h"
+#include "kernel/stat.h"
+int main()
+{
+    int fd;
+    fd = open("shappy.txt", O_CREATE | O_RDWR);
+    if(fd < 0){
+        printf("create error\n");
+        exit(1);
+    }
+    printf("create shappy.txt success\n");
+    write(fd, "shappy\n", 7);
+    return 0;
+}
